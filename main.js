a223b94a5f96b968c6d97bccdc84a5772fd1cd2f
Vladiyar <vladiyar.n@gmail.com>
2022-03-30 00:10:01 +0300

    2

--- main.js
+++ main.js
@@ -1,41 +1,84 @@
 const allowedChars = ['a', 'b', 'c', 'A', 'B', 'C']; // can be generated automatically
 
 function login(password) {
-	return password === "CCCCCC";
+	return password === "CaCbBA";
 }
 
 function brute(arrayOfChars){
-	arrayOfChars.forEach(element => {
-  
-    arrayOfChars.forEach(element1 => {
 
-      arrayOfChars.forEach(element2 => {
 
-      	arrayOfChars.forEach(element3 => {
-        
-      	  arrayOfChars.forEach(element4 => {
-          
-      	  	arrayOfChars.forEach(element5 => {
+	for(let element of arrayOfChars) {
+		if (login(element)) {
+			return console.log('possword is ' + element);
 
-      				      if(login(element + element1 + element2 + element3 + element4 + element5)){
-        							return console.log('possword is ' + element + element1 + element2 + element3 + element4 + element5);
-                    }
+		}
+	}
+	for(let element of arrayOfChars) {
+		for (let element1 of arrayOfChars) {
+			if (login(element + element1)) {
+				return console.log('possword is ' + element + element1);
 
-      
-    				})
-      
-    			})
-      
-    		})
-       			
-      })
-      
-    })   
+			}
+		}
 
-  });
-  
-}
+	}
+		for (let element of arrayOfChars) {
+			for (let element1 of arrayOfChars) {
+				for (let element2 of arrayOfChars) {
+					if (login(element + element1 + element2)) {
+						return console.log('possword is ' + element + element1 + element2);
 
+					}
+				}
+			}
+
+		}
+			for (let element of arrayOfChars) {
+				for (let element1 of arrayOfChars) {
+					for (let element2 of arrayOfChars) {
+						for (let element3 of arrayOfChars) {
+							if (login(element + element1 + element2 + element3)) {
+								return console.log('possword is ' + element + element1 + element2 + element3);
+
+							}
+						}
+					}
+				}
+			}
+
+				for (let element of arrayOfChars) {
+					for (let element1 of arrayOfChars) {
+						for (let element2 of arrayOfChars) {
+							for (let element3 of arrayOfChars) {
+								for (let element4 of arrayOfChars) {
+									if (login(element + element1 + element2 + element3 + element4)) {
+										return console.log('possword is ' + element + element1 + element2 + element3 + element4);
+
+									}
+								}
+							}
+						}
+					}
+
+					for (let element of arrayOfChars) {
+						for (let element1 of arrayOfChars) {
+							for (let element2 of arrayOfChars) {
+								for (let element3 of arrayOfChars) {
+									for (let element4 of arrayOfChars) {
+										for (let element5 of arrayOfChars) {
+											if (login(element + element1 + element2 + element3 + element4 + element5)) {
+												return console.log('possword is ' + element + element1 + element2 + element3 + element4 + element5);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+
+
+			}
 
 
 brute(allowedChars);
