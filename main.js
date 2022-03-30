c2a2581a25fd902e60fe0d95c16f59fb3178a759
Vladiyar <vladiyar.n@gmail.com>
2022-03-30 22:32:26 +0300

    Working version

--- main.js
+++ main.js
@@ -1,41 +1,60 @@
-const allowedChars = ['a', 'b', 'c', 'A', 'B', 'C']; // can be generated automatically
+const allowedChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'A', 'B', 'C', 'D', 'E', 'F', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const maxLength = 10;
 
 function login(password) {
-	return password === "CCCCCC";
-}
-
-function brute(arrayOfChars){
-	arrayOfChars.forEach(element => {
-  
-    arrayOfChars.forEach(element1 => {
-
-      arrayOfChars.forEach(element2 => {
-
-      	arrayOfChars.forEach(element3 => {
-        
-      	  arrayOfChars.forEach(element4 => {
-          
-      	  	arrayOfChars.forEach(element5 => {
-
-      				      if(login(element + element1 + element2 + element3 + element4 + element5)){
-        							return console.log('possword is ' + element + element1 + element2 + element3 + element4 + element5);
-                    }
-
-      
-    				})
-      
-    			})
-      
-    		})
-       			
-      })
-      
-    })   
-
-  });
-  
+return password === "99Ac9aa";
 }
 
 
+function brute(maxLength = 10) {
+	for (let passwordLength = 1; passwordLength <= maxLength; passwordLength++) {
+		let passwordArray = createPasswordArray(passwordLength);
 
-brute(allowedChars);
+
+		do {
+			let password = createPasswordFromArray(passwordArray);
+			if (login(password)) {
+				return password;
+			}
+
+			passwordArray = getNextPasswordArray(passwordArray);
+		} while (passwordArray);
+	}
+	return null;
+
+}
+
+function createPasswordArray(arrayLength) {
+	const passwordArray = [];
+	for (let i = 0; i < arrayLength; i++) {
+		passwordArray.push(0);
+	}
+	return passwordArray;
+}
+
+
+function createPasswordFromArray(passwordArray){
+	let password = '';
+	for (let i = 0; i < passwordArray.length; i++) {
+		password += allowedChars[passwordArray[i]];
+	}
+	return password;
+}
+
+function getNextPasswordArray(passwordArray) {
+	for(let i = passwordArray.length -1; i >= 0; i--) {
+		if (passwordArray[i] < allowedChars.length) {
+			passwordArray[i]++;
+			return passwordArray;
+		}
+		passwordArray[i] = 0;
+	}
+
+	return null
+
+}
+console.time();
+console.log(brute());
+console.timeEnd();
+
+
