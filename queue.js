7257d79ed974c2e075d4bf23c2b4e70326c8295b
Vladiyar N <vladiyar.n@gmail.com>
2022-04-02 19:42:03 +0300

    Update queue.js

--- queue.js
+++ queue.js
@@ -1,55 +1,65 @@
+let test = 0
 async function payload() {
     return new Promise((resolve) => {
-        setTimeout(() => {console.log(123)},  5000);
+        setTimeout(() => {
+            resolve('potato ' + test)
+            test++
+        },  Math.random() * 2000);
 
     })
 }
 
 class Queue {
-   // concurrency = 20;
-    promises = [];
+    concurrency;
+    tasks = [];
     qSize = 0;
 
-    constructor(concurrency = 20) {
+    constructor(concurrency = 5) {
         this.concurrency = concurrency;
     }
 
-    get fullQueue() {
+    isFullQueue() {
         return this.qSize === this.concurrency;
     }
 
-    enqueue(item) {
-        if(this.concurrency === null || this.promises.length < this.concurrency) {
-            return this.promises.push(item);
-        }
-        return this.fullQueue;
+    enqueue(task) {
+      this.tasks.push(task);
+      if (this.isFullQueue()) {
+        return;
+      }
+      this.#execute();
     }
 
-    fulfilled() {
-        console.log('fulfilled')
-        return this.qSize -= 1;
+    fulfilled(value) {
+        console.log(value + ' fulfilled')
+        this.qSize -= 1;
+        this.#execute()
     }
 
     trim() {
-        this.promises.shift();
+        this.tasks.shift();
     }
-    logging() {
+
+    #execute() {
+        if (this.tasks.length < 1) {
+            return;
+        }
         this.qSize += 1;
-        return this.promises[0];
+        this.tasks[0].call().then(
+            (value) => {
+                console.log(value, 'this is the task length right now ' + this.tasks.length + ' and qSize is ' + this.qSize)
+                this.fulfilled(value);
+            });
+        this.trim()
+    }
+
+    onResolve() {
     }
 }
 
 const q = new Queue();
 
-for(let i = 0; i < 40; i++){
+for(let i = 0; i < 10; i++){
     q.enqueue(payload);
-    if (!q.fullQueue) {
-        q.logging().call().then(q.fulfilled());
-        q.trim();
-    }
-    console.log(q.fullQueue);
 }
-
-
-
 
